9a01ec85a3167909c19011580d07a5967256fdad
Gabriel Mendonça <gabriemdo26@gmail.com>
2026-09-11 10:43:44 -0400

    Arrumado o site

--- ImageProject/imagelite/app/Galeria/page.tsx
+++ ImageProject/imagelite/app/Galeria/page.tsx
@@ -3,40 +3,51 @@
 //import { Template } from '../components/Template';
 //import { ImageCard } from '../components/Image';
 import { Template, ImageCard,  } from '@/components';
-import { ImageService, useImageService } from '@/resource/service';
-import { Image } from '../resource/service'; 
+import { ImageService, useImageService} from '@/resource/service';
+import { Image } from '@/resource/image'; 
 import { useState } from 'react';
 
 
 
 
 export default function Galeria() {
-  
-  const useService = useImageService()
-  const[images, setImages] = useState<ImageService[]>([])
+
+  const useService = useImageService();
+
+  const [images, setImages] = useState<Image[]>([]);
 
   async function searchImages() {
     const result = await useService.buscar();
+
     setImages(result);
-    console.table(result)
+
+    console.table(result);
   }
 
-  
-
   return (
-   
-      <Template>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={searchImages}>
-          Mudar Imagem
-        </button>
-          <section className="grid grid-cols-3 gap-4  p-4">
-            
-            <ImageCard  imageName='{images[0]?.name}'/>
-          </section>
-          
-       
-        
-      </Template>
-   
-  )
-}  
+    <Template>
+
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={searchImages}
+      >
+        Mudar Imagem
+      </button>
+
+      <section className="grid grid-cols-3 gap-4 p-4">
+
+        {images.map((image, index) => (
+          <ImageCard
+            key={index}
+            imageName={image.name}
+            imageUrl={image.url}
+            imageSize={image.size?.toString()}
+            uploadDate={image.uploadDate}
+          />
+        ))}
+
+      </section>
+
+    </Template>
+  );
+}
