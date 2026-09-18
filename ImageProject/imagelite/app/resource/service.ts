89f57ae5b82f671a2d60194844a2c93621f7ee66
Gabriel Mendonça <gabriemdo26@gmail.com>
2026-09-18 13:12:09 -0400

    18.09

--- ImageProject/imagelite/app/resource/service.ts
+++ ImageProject/imagelite/app/resource/service.ts
@@ -1,13 +1,12 @@
-import { Image } from "./image";  
-
+import { Image } from "./image";
 export class ImageService {
   baseURL: string = 'http://localhost:8080/images';
 
-  async buscar(): Promise<Image[]> {
-    const response = await fetch(this.baseURL);
-    return  await response.json();
+  async buscar(query: string = '', extension?: string): Promise<Image[]> {
+    const url = `${this.baseURL}?query=${query}&extension=${extension}`;
+    const response = await fetch(url);
+    return await response.json();
   }
 }
-// React Hook
 export const useImageService = () => new ImageService();
- 
+
